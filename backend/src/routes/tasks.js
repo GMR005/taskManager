ba600d5319ccc2142e34a5f411947d01508fda96
GalyameevMihail <mihailgalemeev@mail.ru>
2026-06-05 23:28:04 +0500

    feat: add CRUD

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -16,4 +16,49 @@ router.post ('/', async (req, res) => {
     } catch (err) {
         res.status(500).json({error: err.message})
     }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query (
+            'SELECT * FROM tasks ORDER BY created_at DESC'
+        );
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.put ('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {status} = req.body;
+        const result = await pool.query (
+            'UPDATE tasks SET status = 1$ where id = 2$ RETURNING *',
+            [status, id]
+        );
+        if (result.rows.length===0) {
+            return res.status(404).json({error: 'задача не найдена'});
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 })
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await pool.query(
+            'DELETE FROM tasks WHERE id = $1 RETURNING *',
+            [id]
+        )
+        if (result.rows.length === 0) {
+            return res.status(404).json({error: 'задача не найдена'});
+        }
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+})
+
+module.exports = router;
