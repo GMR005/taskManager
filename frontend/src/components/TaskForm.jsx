95ca9f7b8342f05a36f404a7ea5eeb6501b98885
GalyameevMihail <mihailgalemeev@mail.ru>
2026-06-07 18:05:42 +0500

    feat: add TaskForm

---
+++ frontend/src/components/TaskForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default function TaskForm({onTaskCreated}) {
+    const[title, setTitle] = useState('');
+    const [description, setDescriprion] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!title.trim()) return    
+
+        await fetch('/tasks', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title, description})
+        })
+
+        setTitle('');
+        setDescriprion('');
+        onTaskCreated();
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Название задачи"
+                required
+            />
+            <input 
+                value={description}
+                onChange={(e) => setDescriprion(e.target.value)}
+                placeholder="Описание (необязательно"
+            />
+            <button type = "submit">Создать</button>
+        </form>
+    )
+}
