453e87e6b83be4a2401d41a8dd060687fe6a969d
GalyameevMihail <mihailgalemeev@mail.ru>
2026-06-12 18:53:43 +0500

    feat(db): add users table and user_id foreign key for tasks

--- backend/src/db.js
+++ backend/src/db.js
@@ -10,17 +10,41 @@ const pool = new Pool ({
 
 const initDB = async () => {
     try {
-        await pool.query(`CREATE TABLE IF NOT EXISTS tasks (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        description TEXT,
-        status VARCHAR(20) DEFAULT 'new' CHECK (status IN ('new', 'in_progress', 'done')),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
-    `)
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR (255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `)
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS tasks (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            status VARCHAR(20) DEFAULT 'new' CHECK (status IN ('new', 'in_progress', 'done')),
+            user_id INTEGER REFERENCES userd(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `)
+
+        const check = await pool.query(`
+            SELECT column_name FROM information_schema.columns
+            WHERE table_name = 'tasks' AND column_name = 'user_id'
+        `);
+
+        if (check.rows.length===0) {
+            await pool.query (
+                'ALTER TABLE tasks ADD COLUMN user_id INTEGER REFERENCES users(id)'
+            )
+        }
+       console.log ('база создана');     
     } catch (err) {
         console.error('ошибка инициализации бд:', err.message);
     }
+
 }
 
 module.exports = {pool, initDB};
